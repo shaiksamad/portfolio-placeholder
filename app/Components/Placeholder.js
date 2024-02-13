e30b83dcd7ee4819ef2c868a2ba30234c8d26344
Samad <41123191+shaiksamad@users.noreply.github.com>
2024-02-13 23:31:33 +0530

    added placeholder

---
+++ app/Components/Placeholder.js
@@ -0,0 +1,19 @@
+export default function Placeholder() {
+    return <>
+    <main className="">
+        <div id="placeholder-container" className="">
+            <div id="placeholder-header">
+                <h1 id="title">Hi, I am Samad</h1>
+                <p id="description">I make Python Automation Scripts, Full Stack web apps, Backend using Flask and REST API.</ p>
+            </div>
+            <div id="divider"></div>
+            <div id="placeholder-body">
+       
+                <p>LinkedIn: <a target="_blank" href="https://linkedin.com/in/shaiksamad">linkedin.com/in/shaiksamad</a></p>
+                <p>GitHub: <a target="_blank" href="https://github.com/shaiksamad">github.com/shaiksamad</a></p>
+                <p>Mail: <a target="_blank" href="mailto:shaiksamad.dev@gmail.com">shaiksamad.dev@gmail.com</a></p>
+            </div>
+        </div>
+    </main>
+    </>
+}
